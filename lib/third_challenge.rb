d6f337908a4624ceb25c286831c307f339fbdf39
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 06:58:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,12 +27,19 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name =>
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Juliet",
+          :age => "15",
+          :status => "alive"
+        }
+      ]
    }
   }
 
